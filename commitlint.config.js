9b479f0cc6062ae242f364c6b24b865e59c8c71c
Jsmond2016 <jsmond2016@gmail.com>
2021-03-07 15:20:18 +0800

    udate package.json

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,1 @@
-// commitlint.config.js// 配置
-rulesmodule.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
-    ],
-    "scope-empty": [1, "never"],
-    "subject-case": [0, "always"],
-    "scope-case": [0],
-  },
-};
+module.exports = { extends: ["@commitlint/config-conventional"] };
